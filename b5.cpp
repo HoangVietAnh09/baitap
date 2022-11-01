e99a52d144e54759c2d9d2fb47b5e5ccd03efa7a
HoangVietAnh09 <111860567+HoangVietAnh09@users.noreply.github.com>
2022-11-01 11:42:46 +0700

    s5

---
+++ b5.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int tong(int n)
+{
+	int sum = 0;
+	for(int i = 1; i <= n; i++)
+	{
+		sum += i;
+	}
+	return sum;
+}
+int main()
+{
+	int n;
+	cin >> n;
+	int sum = 0;
+	for(int i = 1; i <= n; i++)
+	{
+		sum += tong(i);
+	}
+	cout << sum;
+	return 0;
+}
